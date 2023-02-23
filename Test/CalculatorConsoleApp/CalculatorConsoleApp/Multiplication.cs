9bdc92b4fff19fd8ad42a81b77142f394dde8c29
Rajmote <Rajmote>
2023-02-23 16:10:15 +0530

    Added project to test and made model change. Its a working model.

---
+++ Test/CalculatorConsoleApp/CalculatorConsoleApp/Multiplication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+    public class Multiplication
+    {
+        public int Divide(int Num1, int Num2)
+        {
+            return Num1 * Num2;
+        }
+    }
+}
